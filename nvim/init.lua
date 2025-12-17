d6db0f8ec428908f5f3d724a82a9a2ea32f53dd7
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-12-17 21:39:26 +0100

    neovim: update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,6 @@
 require("plugins.gruber-darker")
 require("plugins.naysayer")
 
---[[ TODO:
-	[x] Sane options
-	[x] Autopairs
-	[x] File finder
-	[x] Tree-sitter
-	[~] Buffer management
-	[x] LSP + Autocompletion
-	[x] Linter
-	[x] Formatting
-	[x] Git?
-	[x] Keybindings
-	[?] Custom status line
---]]
-
 require("opts")
 
 require("plugins.autopairs")
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,8 +4,7 @@ vim.pack.add({
 
 require('nvim-treesitter').setup()
 
-require('nvim-treesitter').install({ "rust", "c", "bash", "bibtex", "cmake", "commonlisp", "cpp", "csv", "diff", "disassembly", "dockerfile", "doxygen", "haskell", "json", "toml", "lua", "markdown", "meson", "ninja", "nix", "python", "regex", "tcl",
-"tmux", "tsx", "systemverilog", "typst", "vhdl", "vim", "vimdoc", "xml", "yaml", "zig", "zsh" })
+-- require('nvim-treesitter').install({ "rust", "c", "bash", "bibtex", "cmake", "commonlisp", "cpp", "csv", "diff", "disassembly", "dockerfile", "doxygen", "haskell", "json", "toml", "lua", "markdown", "meson", "ninja", "nix", "python", "regex", "tcl", "tmux", "tsx", "systemverilog", "typst", "vhdl", "vim", "vimdoc", "xml", "yaml", "zig", "zsh" })
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
